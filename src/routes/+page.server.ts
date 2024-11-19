ece970036ab3b643aafdc07fc2d3f8e01dd3459c
PurelyAnecdotal <58897346+PurelyAnecdotal@users.noreply.github.com>
2024-11-19 12:15:10 -0800

    add chat ui template

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,7 @@ import { db } from '$lib';
 
 export function load() {
 	const result = db.query(`SELECT COUNT(*) FROM books`).get() as { 'COUNT(*)': number };
+    
 	return {
 		count: result['COUNT(*)']
 	};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,4 +6,13 @@
 
 <h1>DestinyChat</h1>
 
+<div class="m-2 flex max-w-md flex-col gap-2 rounded bg-slate-200 p-2">
+	<div class="w-fit rounded bg-slate-300 p-2">asdf</div>
+	<div class="ml-auto w-fit rounded bg-slate-800 p-2 text-white">asdf</div>
+	<div class="flex rounded bg-slate-300 p-2">
+		<input class="w-full bg-slate-1300" />
+		<button class="w-fit rounded bg-slate-800 p-2 text-white">Send</button>
+	</div>
+</div>
+
 {data.count} books
